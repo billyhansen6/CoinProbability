0070ac987fcbd986cd42055c9a22b6d94f2402ef
Billy Hansen <bhansen@nsareit.net>
2020-07-27 07:49:41 -0700

    Simplify init function

--- Coins_Load_Data.py
+++ Coins_Load_Data.py
@@ -1,7 +1,7 @@
-class Coins_Load_Data:
+class CoinsLoadData:
 
     def __init__(self, A_count = 10, B_count = 10):
-        """Generic coin collection for cacluating basic probabilities.
+        """Generic coin collection for calculating basic probabilities.
 
         Attributes:
             A_count (int) representing the number of A coins in a collection.
@@ -9,7 +9,6 @@ class Coins_Load_Data:
             n (int) representing the total coins in the collection
             data_list (list of strings) a list of A and B coins representing a coin collection.
             """
-
         self.A_count = A_count
         self.B_count = B_count
         self.n = A_count + B_count
